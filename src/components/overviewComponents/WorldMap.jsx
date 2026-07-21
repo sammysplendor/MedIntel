a915a8a9c9d790838605833c9457baea05e6dbf6
sammysplendor <splendidsam006@gmail.com>
2026-07-21 01:39:44 +0100

    Make map zoomable

--- src/components/overviewComponents/WorldMap.jsx
+++ src/components/overviewComponents/WorldMap.jsx
@@ -1,17 +1,26 @@
 import styles from "../../styles/components styles/overview/WorldMap.module.css";
 import { geoUrl } from "../../constants/config";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+} from "react-simple-maps";
 import { useState } from "react";
 import useCountriesDetails from "../../hooks/useCountriesDetails";
 import { getCountryHealthData } from "../../api/worldbankApi";
 
 const WorldMap = () => {
+  const [hoveredCountry, setHoveredCountry] = useState("");
+
+  // ===== COUNTRY SELECTION ON MAP ===== //
   const { countries } = useCountriesDetails();
 
   const [selectedCountry, setSelectedCountry] = useState(null);
 
   const handleCountryClick = async (geo) => {
     const country = countries.find((c) => c.name === geo.properties.name);
+    console.log(geo.properties);
 
     if (!country) return;
 
@@ -21,26 +30,60 @@ const WorldMap = () => {
     setSelectedCountry(countryData);
   };
 
+  // ===== ZOOM BUTTONS FUNCTIONALITY ===== //
+  const [position, setPosition] = useState({
+    coordinates: [0, 20],
+    zoom: 1,
+  });
+
+  const handleZoomOut = () => {
+    setPosition((prev) => ({
+      ...prev,
+      zoom: Math.max(prev.zoom - 1, 1),
+    }));
+  };
+
+  const handleZoomIn = () => {
+    setPosition((prev) => ({ ...prev, zoom: position.zoom + 1 }));
+  };
+
   return (
     <section className={styles.mapSection}>
       <div className={styles.mapContainer}>
+        <span className={styles.zoomBtns}>
+          <button onClick={handleZoomOut} title="Zoom out">
+            -
+          </button>
+          <button onClick={handleZoomIn} title="Zoom in">
+            +
+          </button>
+        </span>
+
         <ComposableMap
           projectionConfig={{ scale: 150 }}
           width={900}
           height={400}
         >
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  className={styles.countries}
-                  onClick={() => handleCountryClick(geo)}
-                />
-              ))
-            }
-          </Geographies>
+          <ZoomableGroup
+            zoom={position.zoom}
+            center={position.coordinates}
+            onMoveEnd={(position) => setPosition(position)}
+          >
+            <Geographies geography={geoUrl}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    className={styles.countries}
+                    onMouseEnter={() => setHoveredCountry(geo.properties.name)}
+                    onMouseLeave={() => setHoveredCountry("")}
+                    onClick={() => handleCountryClick(geo)}
+                  />
+                ))
+              }
+            </Geographies>
+          </ZoomableGroup>
         </ComposableMap>
       </div>
 
